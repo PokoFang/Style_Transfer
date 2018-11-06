42cafb24019cfa6e310bc87ca9c06ceb56a62f21
Poko Fang <kogiidae@gmail.com>
2018-11-06 13:49:03 +0800

    add gram matrix
    Add gram matrix

--- model.py
+++ model.py
@@ -7,11 +7,18 @@ from PIL import Image
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-content_image = scipy.misc.imread('image/content.jpg')
-
 def cal_content_cost(a_G, a_C):
 	# a_G.shape returns a tensor of shape, and needs to run sess to get the value
 	# a_G.get_shape() returns a tuple
 	m, n_h, n_w, n_c = a_G.get_shape().as_list()
 	J_content = tf.reduce_sum(tf.square(tf.subtract(a_G, a_C))) / (4 * n_h * n_w * n_c)
 	return J_content
+	
+def gram_matrix(matrix):
+	matrix_T = tf.matrix_transpose(matrix)
+	G_matrix = tf.matmul(matrix_T, matrix)
+	return G_matrix
+
+if __name__ == '__main__':
+	content_img = scipy.misc.imread('image/content.jpg')
+	style_img = scipy.misc.imread('image/style.jpg')
--- unittest_model.py
+++ unittest_model.py
@@ -2,19 +2,26 @@ import unittest
 import model
 import tensorflow as tf
 import os
+import numpy as np
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 
 class ModelTestCase(unittest.TestCase):
 	def test_cal_content_cost(self):
 		expected = 0.25
 		tf.reset_default_graph()
 		with tf.Session() as test:
-			tf.set_random_seed(1)
 			x = tf.ones([1, 4, 4, 3], dtype=tf.float32)
 			y = tf.multiply(tf.ones([1, 4, 4, 3], dtype=tf.float32), 2)
 			result = test.run(model.cal_content_cost(x, y))
 		self.assertEquals(expected, result)
+
+	def test_gram_matrix(self):
+		expected = np.array([[10, 14], [14, 20]])
+		tf.reset_default_graph()
+		with tf.Session() as test:
+			x = tf.constant([[1, 2], [3, 4]])
+			result = test.run(model.gram_matrix(x))
+		self.assertTrue((expected == result).all())		
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
