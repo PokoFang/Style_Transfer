c386d9a739c71221ad0e4be85e72ba9cedc38eec
Poko Fang <kogiidae@gmail.com>
2018-11-08 13:33:02 +0800

    add style cost for one layer
    Calculate the style cost for one layer

--- model.py
+++ model.py
@@ -11,13 +11,25 @@ def cal_content_cost(a_G, a_C):
 	# a_G.shape returns a tensor of shape, and needs to run sess to get the value
 	# a_G.get_shape() returns a tuple
 	m, n_h, n_w, n_c = a_G.get_shape().as_list()
-	J_content = tf.reduce_sum(tf.square(tf.subtract(a_G, a_C))) / (4 * n_h * n_w * n_c)
-	return J_content
+	content_cost = tf.reduce_sum(tf.square(tf.subtract(a_G, a_C))) / (4 * n_h * n_w * n_c)
+	return content_cost
 	
 def gram_matrix(matrix):
 	matrix_T = tf.matrix_transpose(matrix)
-	G_matrix = tf.matmul(matrix_T, matrix)
+	G_matrix = tf.matmul(matrix, matrix_T)
 	return G_matrix
+
+def cal_layer_style_cost(a_G, a_S): # For a specific layer, the similarity between each channel's activative values
+	m, n_h, n_w, n_c = a_G.get_shape().as_list()
+	a_G = tf.reshape(tf.transpose(a_G), [n_c, -1]) # flatten 4d matrix to 2d matrix
+	a_S = tf.reshape(tf.transpose(a_S), [n_c, -1])
+
+	GG = gram_matrix(a_G) # gram matrix of generated matrix
+	GS = gram_matrix(a_S) 
+
+	style_cost = tf.reduce_sum(tf.square(tf.subtract(GG, GS))) / (4 * (n_c * n_h * n_w) ** 2)
+	return style_cost
+
 
 if __name__ == '__main__':
 	content_img = scipy.misc.imread('image/content.jpg')
--- unittest_model.py
+++ unittest_model.py
@@ -16,12 +16,24 @@ class ModelTestCase(unittest.TestCase):
 		self.assertEquals(expected, result)
 
 	def test_gram_matrix(self):
-		expected = np.array([[10, 14], [14, 20]])
+		expected = np.array([[5, 11], [11, 25]])
 		tf.reset_default_graph()
 		with tf.Session() as test:
 			x = tf.constant([[1, 2], [3, 4]])
 			result = test.run(model.gram_matrix(x))
 		self.assertTrue((expected == result).all())		
+
+	def test_cal_layer_style_cost(self):
+		expected = 2.25
+		tf.reset_default_graph()
+		with tf.Session() as test:
+			tf.set_random_seed(1)
+			x = tf.ones([1, 4, 4, 3], dtype=tf.float32)
+			y = tf.multiply(tf.ones([1, 4, 4, 3], dtype=tf.float32), 2)
+			result = test.run(model.cal_layer_style_cost(x, y))
+			print(result)
+		self.assertEquals(expected, result)
+
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
